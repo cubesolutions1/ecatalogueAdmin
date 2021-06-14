3be04c0acbde2d2fa2d6b19c7bed89c6871f3020
rhimiirami <rhimiirami@gmail.com>
2021-06-14 10:23:41 +0200

    fixConflit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,17 +6,20 @@
 export const environment = {
   production: false,
 
-  // apiUrl: 'http://192.168.1.64:3112/api/v1/',
-  //  apiUrl: 'https://admin.neod-distri.fr:3116/api/v1/',
+   apiUrl: 'http://192.168.1.80:3112/api/v1/',
+  // apiUrl: 'https://admin.neod-distri.fr:3116/api/v1/',
+ // apiUrl: 'http://192.168.43.11:3112/api/v1/',
   // apiUrl: 'https://192.168.1.8:3112/api/v1/',
-  apiUrl: 'http://catalogue.cubesolutions.tn:3112/api/v1/',
+ // apiUrl: 'http://catalogue.cubesolutions.tn:3112/api/v1/',
    // apiUrl: 'http://192.168.1.39:3112/api/v1/',
 
-  //apiImg :'https://admin.neod-distri.fr:3116/img/',
-  // apiImg: 'http://192.168.1.64:3112/img/',
+ //apiImg :'https://admin.neod-distri.fr:3116/img/',
+   apiImg: 'http://192.168.1.80:3112/img/',
   // apiImg: 'https://192.168.1.8:3112/img/',
-  apiImg: 'http://catalogue.cubesolutions.tn:3112/img/',
+//  apiImg: 'http://catalogue.cubesolutions.tn:3112/img/',
  // apiImg: 'http://192.168.1.39:3112:3112/img/',
+ //apiImg: 'http://192.168.43.11:3112/img/',
+
 
 
 
